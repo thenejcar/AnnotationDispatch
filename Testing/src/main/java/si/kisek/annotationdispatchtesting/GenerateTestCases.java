ed5aed05d7243b68da4e67fa868f854437b29009
Nejc Kisek <nejc@kisek.si>
2018-09-27 11:27:01 +0200

    fixed generating of some of the Void tests

--- Testing/src/main/java/si/kisek/annotationdispatchtesting/GenerateTestCases.java
+++ Testing/src/main/java/si/kisek/annotationdispatchtesting/GenerateTestCases.java
@@ -18,6 +18,18 @@ public class GenerateTestCases {
     private static final String GENERATED_PATH = "generated-tests/src/main/java/";
 
 
+    private static void write(String filename, String content) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            System.out.println("    wrote " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
         // number of total calls = nMulitmethods * nCallsEach
@@ -26,73 +38,34 @@ public class GenerateTestCases {
         System.out.println("Generating test: Parameters");
         for (int i : new int[] {1, 2, 3, 5, 10, 15, 20}) {
             String str = Generator.generateTestClass("Parameters" + i, 1, 5, 100, i, 3, 3, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "Parameters" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            write(GENERATED_PATH + "Parameters" + i + ".java", str);
         }
 
         // testing the number of classes available -- don't overdo this, or we get COMPILATION ERROR: too many constants
         System.out.println("Generating test: Classes");
         for (int i : new int[] {1, 2, 3, 4, 5, 8, 10}) {
             String str = Generator.generateTestClass("Classes" + i, 1, 5, 100, 5, 3, i, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "Classes" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            write(GENERATED_PATH + "Classes" + i + ".java", str);
         }
+
         System.out.println("Generating test: ClassesWidth");
         for (int i : new int[] {1, 2, 3, 4, 5, 8, 10}) {
             String str = Generator.generateTestClass("ClassesWidth" + i, 1, 5, 100, 5, 3,  4, i, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "ClassesWidth" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            write(GENERATED_PATH + "ClassesWidth" + i + ".java", str);
         }
 
         // testing the number of multimethods
         System.out.println("Generating test: Methods");
         for (int i : new int[] {1, 2, 4, 5, 10, 20, 25}) {
             String str = Generator.generateTestClass("Methods" + i, i, 5, (100 / i), 5, 3, 3, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "Methods" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            write(GENERATED_PATH + "Methods" + i + ".java", str);
         }
 
         // testing the number of instances
         System.out.println("Generating test: Instances");
         for (int i : new int[] {1, 2, 4, 5, 10, 20, 25}) {
             String str = Generator.generateTestClass("Instances" + i, 1, i, 100, 5, 3, 3, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "Instances" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            write(GENERATED_PATH + "Instances" + i + ".java", str);
         }
 
 
@@ -101,61 +74,29 @@ public class GenerateTestCases {
         System.out.println("Generating test: ParametersVoid");
         for (int i : new int[] {1, 2, 3, 5, 10, 15, 20}) {
             String str = Generator.generateTestClass("ParametersVoid" + i, 1, 5, 100, i, 3, 3, 3, true);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "ParametersVoid" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            write(GENERATED_PATH + "ParametersVoid" + i + ".java", str);
         }
 
         // testing the number of classes available -- don't overdo this, or we get COMPILATION ERROR: too many constants
         System.out.println("Generating test: ClassesVoid");
         for (int i : new int[] {1, 2, 3, 4, 5, 8, 10}) {
-            String str = Generator.generateTestClass("ClassesVoid" + i, 1, 5, 100, 5, 3, i, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "ClassesVoid" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String str = Generator.generateTestClass("ClassesVoid" + i, 1, 5, 100, 5, 3, i, 3, true);
+            write(GENERATED_PATH + "ClassesVoid" + i + ".java", str);
         }
 
 
         // testing the number of multimethods
         System.out.println("Generating test: MethodsVoid");
         for (int i : new int[] {1, 2, 4, 5, 10, 20, 25}) {
-            String str = Generator.generateTestClass("MethodsVoid" + i, i, 5, (100 / i), 5, 3, 3, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "MethodsVoid" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String str = Generator.generateTestClass("MethodsVoid" + i, i, 5, (100 / i), 5, 3, 3, 3, true);
+            write(GENERATED_PATH + "MethodsVoid" + i + ".java", str);
         }
 
         // testing the number of instances
         System.out.println("Generating test: InstancesVoid");
         for (int i : new int[] {1, 2, 4, 5, 10, 20, 25}) {
-            String str = Generator.generateTestClass("InstancesVoid" + i, 1, i, 100, 5, 3, 3, 3, false);
-
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(GENERATED_PATH + "InstancesVoid" + i + ".java"));
-                writer.write(str);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String str = Generator.generateTestClass("InstancesVoid" + i, 1, i, 100, 5, 3, 3, 3, true);
+            write(GENERATED_PATH + "InstancesVoid" + i + ".java", str);
         }
     }
 
